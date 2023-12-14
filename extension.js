82a5a873aed6a3cd6ef810947036bdf22edb28fa
Asuka Minato <i@asukaminato.eu.org>
2023-12-14 09:40:22 +0900

    Hover (#4)
    * add primitive hover
    
    * match E
    
    * bump version

--- extension.js
+++ extension.js
@@ -3,7 +3,9 @@
 const vscode = require("vscode");
 const path = require("path");
 const fs = require("fs");
-
+/**
+ * @type {Object.<string, string>}
+ */
 const allFunctionNames = Object.assign(
   {},
   Object.fromEntries(
@@ -28,20 +30,40 @@ const allFunctionNames = Object.assign(
  */
 function activate(context) {
   context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider("wolfram", {
-      provideCompletionItems(document, position, token, context) {
-        // return all completion items as array
-        return Object.entries(allFunctionNames).map(([name, desc]) => {
-          let c = new vscode.CompletionItem(
-            name,
-            vscode.CompletionItemKind.Function
+    vscode.languages.registerCompletionItemProvider(
+      { scheme: "file", language: "wolfram" },
+      {
+        provideCompletionItems(document, position, token, context) {
+          // return all completion items as array
+          return Object.entries(allFunctionNames).map(([name, desc]) => {
+            const c = new vscode.CompletionItem(
+              name,
+              vscode.CompletionItemKind.Function
+            );
+            c.insertText = name;
+            c.documentation = desc;
+            return c;
+          });
+        },
+      }
+    ),
+    vscode.languages.registerHoverProvider(
+      { scheme: "file", language: "wolfram" },
+      {
+        provideHover(document, position, token) {
+          return new vscode.Hover(
+            "```mma\n" +
+              (allFunctionNames[
+                document.getText(
+                  // this regex do a loose match, only check the first is UPPER and ignore others.
+                  document.getWordRangeAtPosition(position, /[A-Z][`\$A-z]*/)
+                )
+              ] || "") +
+              "\n```"
           );
-          c.insertText = name;
-          c.documentation = desc;
-          return c;
-        });
-      },
-    })
+        },
+      }
+    )
   );
 }
 
